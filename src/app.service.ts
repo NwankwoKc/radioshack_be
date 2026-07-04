500a177e2123222f90424e7db9dd95bd021bdedb
NwankwoKc <nkelechi21@gmail.com>
2026-07-04 20:44:13 +0100

    fixing getHello controller to return defaul message

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,12 @@
 
-import { Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AppService {
   constructor(private configservice: ConfigService) { }
+
+
+  helloworld() {
+    return "welcome to radioshack your number 1 audiostreaming app"
+  }
 }
